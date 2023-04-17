f11c09cfdaa41f29a059b0fcd07226771c00787e
PauloMalet <malet.paul@gmail.com>
2023-04-17 09:03:05 +0200

    Commenté

--- client.c
+++ client.c
@@ -10,31 +10,39 @@
 #include <netdb.h>          
 
 
-
+//Definir la structure qui permet d'envoyer le message
 typedef struct{
     char nom[30];
     char message[100]; 
 } User;
 
+
 int main(void){
+    //Creer socket client
     int socketClient = socket(AF_INET, SOCK_STREAM, 0);
-    printf("socketClient : %d\n", socketClient);	//ça ça marche
+    printf("socketClient : %d\n", socketClient);	//ca marche
     struct sockaddr_in addrClient = {0};
     addrClient.sin_addr.s_addr = inet_addr("127.0.0.1");
     addrClient.sin_family = AF_INET;
     addrClient.sin_port = htons(2002);
+    
+    //Connecter socket client au socket serveur
     int id_co = connect(socketClient, (const struct sockaddr*)&addrClient, sizeof(addrClient));
     printf("id_co : %d\n", id_co);	//renvoie -1
     printf("Connexion effectuee\n");
 
     User user;
     char question[25];
+
+    //recption d'un message venant du serveur
     int longueur = recv(socketClient, question, 25, 0);
-    printf("C'est : %d \n", longueur);	//je tombe sur -1 : erreur, j'avais pas protégé et vérifié, c'est ça qui me fait tout planter depuis des heures
+    printf("C'est : %d \n", longueur);	//renvoie -1
     printf("%s\n", question);
     printf("test1 \n");
+
+    //Envoie d'un message au serveur
     scanf("%29s %99s", user.nom, user.message);
-    printf("c'est %s %s \n", user.nom, user.message);	//ça marche, faut avoir un seul espace (donc 2 arguments) quand on tape dans le prompt
+    printf("c'est %s %s \n", user.nom, user.message);	//ca marche, faut avoir un seul espace (donc 2 arguments) quand on tape dans le prompt
     send(socketClient, &user, sizeof(user), 0);
     printf("test3 \n");
 
--- serveur.c
+++ serveur.c
@@ -12,10 +12,12 @@
 
 #define MAX_CLIENTS 10
 
+//Definir la structure qui permet d'envoyer le message
 typedef struct {
     char nom[30];
     char message[100]; 
 } User;
+
 
 void *fonction(void *arg){
     int socket = *(int*) arg;
@@ -23,13 +25,15 @@ void *fonction(void *arg){
     User user;
     send(socket, question, strlen(question)+1,0);   
     recv(socket, &user, sizeof(user), 0);
-    printf("le message du client %s est : %s\n", user.nom, user.message);   //%s ou autre chose ?
+    printf("le message du client %s est : %s\n", user.nom, user.message);       //%s ou autre chose ?
     close(socket);
     free(arg);      //pas sur de quoi a quoi ca sert
     pthread_exit(NULL);
 }
 
+
 int main(void){
+    //Creer socket serveur
     int socketServeur = socket(AF_INET, SOCK_STREAM, 0);
     if (socketServeur  == -1){
 		perror("Error creating socket.\n");
@@ -38,19 +42,22 @@ int main(void){
     struct sockaddr_in addrServeur  = {0};
     addrServeur.sin_addr.s_addr = inet_addr("127.0.0.1.");
     addrServeur.sin_family = AF_INET;
-    addrServeur.sin_port = htons(2003);     //essayer un autre port aussi
+    addrServeur.sin_port = htons(2002);    
 
     //Connection de la socket a l'addresse serveur
     bind(socketServeur, (struct sockaddr *)&addrServeur, sizeof(addrServeur));       
     printf("bind : %d \n", socketServeur);
 
+    //Serveur ecoute
     listen(socketServeur, 2);   //file d'attente au max de 2
     printf("Serveur ecoute \n");
 
+    //Creer thread pour chaque client qui se connecte
     pthread_t threads[2];
     for (int i=0; i<2; i++){
-        struct sockaddr_in addrClient;      //pas bien compris comment ces quelques lignes qui suivent marchent
+        struct sockaddr_in addrClient;
         socklen_t tailleClient = sizeof(addrClient);
+        //Accepter la connection
         int socketClient = accept(socketServeur, (struct sockaddr*)&addrClient, &tailleClient);
         printf("socketClient : %d \n", socketClient);
         printf("Connexion acceptee \n");
@@ -62,6 +69,7 @@ int main(void){
         pthread_create(&threads[i], NULL, fonction, arg);
     }
 
+    //Fermer les threads
     for (int i=0; i<2; i++){
         pthread_join(threads[i], NULL);
     }
